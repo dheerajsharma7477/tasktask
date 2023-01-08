428e029c6cc8fcb67a64a3b420e4e154370cbb37
dheeraj <dheeraj.sharma@insightgeeks.in>
2023-01-08 18:36:36 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { OffersComponent } from './window/offers/offers.component';
 
 const routes: Routes = [
   // {
-  //   path: '',
-  //   component: OffersComponent,
+  //   path: '**',
+  //   loadChildren: () => import('./window/window.module').then(m => m.WindowModule),
   // },
   {
     path: 'tasktask',
@@ -17,7 +17,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
